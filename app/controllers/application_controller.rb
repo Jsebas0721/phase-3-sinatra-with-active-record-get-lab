5e2f00d2902b1b3b9e99af7deb2dafdd7143d7d4
sebastian medina <sebastianlopez_21@hotmail.com>
2023-03-06 10:21:24 -0500

    commit tree 3b30c6ee1c799ba7fbca543281caa3fb3c0c4950

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,39 @@
 class ApplicationController < Sinatra::Base
+  
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    
+    # returns a single bakery as JSON with its baked goods nested in an array. Use the id from the URL to look up the correct bakery. 
+    # (HINT: you'll need to pass in some custom options to the #to_jsonLinks to an external site. method.)
+    bake = Bakery.find(params[:id])
+
+    bake.to_json(include: :baked_goods)
+
+  end
+
+  get '/baked_goods/by_price' do
+    
+    baked_goods = BakedGood.all.order(price: :desc)
+
+    baked_goods.to_json
+
+  end
+
+  get '/baked_goods/most_expensive' do
+    
+    baked_goods = BakedGood.all.order(price: :desc).first
+
+    baked_goods.to_json
+
+  end
 
 end
